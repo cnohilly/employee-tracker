e6e2672ac33a9bdd8c1abe34c685f8cb82d5acb0
ctnohilly <ctnohilly@gmail.com>
2022-07-27 23:04:53 -0400

    created routes for returning all employees, and for returning employees is a better format

--- routes/apiRoutes/employeeRoutes.js
+++ routes/apiRoutes/employeeRoutes.js
@@ -1,5 +1,73 @@
+const db = require('../../db/connection');
 const router = require('express').Router();
 
+router.get('/', (req,res) => {
+    const sql = `SELECT id AS 'ID', first_name AS 'First Name', last_name AS 'Last Name', role_id AS 'Role ID', manager_id AS 'Manager ID'
+                FROM employees`;
+    db.query(sql, (err,rows) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message:'success',
+            data: rows
+        });
+    });
+});
 
+router.get('/format', (req,res) => {
+    const sql = `SELECT emp.id AS 'ID', emp.first_name AS 'First Name', emp.last_name AS 'Last Name', roles.title AS 'Title', departments.dept_name AS 'Department',
+                 roles.salary AS 'Salary', CONCAT(mng.first_name, ' ', mng.last_name) AS 'Manager'
+                FROM employees emp
+                LEFT JOIN roles ON emp.role_id = roles.id
+                LEFT JOIN departments ON roles.department_id = departments.id
+                LEFT JOIN employees mng ON emp.manager_id = mng.id`;
+    db.query(sql, (err,rows) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message:'success',
+            data: rows
+        });
+    });
+});
+
+router.get('/:id', (req,res) => {
+    const sql = `SELECT id AS 'ID', title AS 'Title', salary AS 'Salary', department_id AS 'Dept. ID'
+                FROM roles
+                WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err,row) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message:'success',
+            data: row
+        });
+    });
+});
+
+router.get('/:id/format', (req,res) => {
+    const sql = `SELECT roles.id AS 'ID', roles.title AS 'Title', roles.salary AS 'Salary', departments.dept_name AS 'Dept. Name'
+                FROM roles
+                LEFT JOIN departments ON department_id = departments.id
+                WHERE roles.id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err,row) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message:'success',
+            data: row
+        });
+    });
+});
 
 module.exports = router;

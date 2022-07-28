1c5430163fde278d6fab8e0d886d551515725b5c
ctnohilly <ctnohilly@gmail.com>
2022-07-27 22:30:05 -0400

    added route for finding department by id

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -3,18 +3,35 @@ const db = require('../../db/connection');
 const router = require('express').Router();
 
 router.get('/', (req,res) => {
-    const sql = `SELECT id AS 'ID', dept_name AS Department Name
+    const sql = `SELECT id AS 'ID', dept_name AS 'Department Name'
                 FROM departments`;
-    db.query(sql, (err,row) => {
+    db.query(sql, (err,rows) => {
         if(err) {
             res.status(400).json({error: err.message});
             return;
         }
         res.json({
             message:'success',
-            data:row
+            data: rows
         });
     });
 });
+
+router.get('/:id', (req,res) => {
+    const sql = `SELECT id AS 'ID', dept_name AS 'Department Name'
+                FROM departments
+                WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err,row) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message:'success',
+            data: row
+        });
+    })
+})
 
 module.exports = router;
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const employeeRoutes = require('./employeeRoutes');
+const departmentRoutes = require('./departmentRoutes');
 
-router.use('/employee', employeeRoutes);
+router.use('/employees', employeeRoutes);
+router.use('/departments', departmentRoutes);
 
 module.exports = router;

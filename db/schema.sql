bf1f18efdbe439475e67d2374bcc0abd3b3e11cf
ctnohilly <ctnohilly@gmail.com>
2022-07-27 21:38:43 -0400

    created seeds file to create test data

--- db/schema.sql
+++ db/schema.sql
@@ -19,9 +19,9 @@ CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  roles_id INTEGER NOT NULL,
+  role_id INTEGER NOT NULL,
   manager_id INTEGER,
-  CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE CASCADE,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
 

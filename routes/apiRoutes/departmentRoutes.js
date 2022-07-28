b044732ffb8df80c0814f2a48122f9283755949d
ctnohilly <ctnohilly@gmail.com>
2022-07-27 21:58:33 -0400

    base route for the departments api routes

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -1,5 +1,20 @@
+const db = require('../../db/connection');
+
 const router = require('express').Router();
 
-
+router.get('/', (req,res) => {
+    const sql = `SELECT id AS 'ID', dept_name AS Department Name
+                FROM departments`;
+    db.query(sql, (err,row) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message:'success',
+            data:row
+        });
+    });
+});
 
 module.exports = router;
